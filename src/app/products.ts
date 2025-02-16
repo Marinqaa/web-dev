e49b23095a80683fe7302861ee0118e3c14329a1
Marina <123238442+Marinqaa@users.noreply.github.com>
2025-02-16 19:49:13 +0500

    Add files via upload

---
+++ src/app/products.ts
@@ -0,0 +1,133 @@
+export const products=[
+  {
+    name:'Apple iPhone 13 128Gb черный',
+    description:'Apple iPhone 13 получил дисплей 6.1 дюйма Super Retina XDR, который отличается невероятно высокой плотностью пикселей — фотографии, видео и текст выглядят поразительно четко. ',
+    link:"https://kaspi.kz/shop/p/apple-iphone-13-128gb-chernyi-102298404/?c=750000000",
+    imgURL:'https://resources.cdn-kaspi.kz/img/m/p/h32/h70/84378448199710.jpg?format=gallery-large',
+    gallery: [
+      'https://resources.cdn-kaspi.kz/img/m/p/h32/h70/84378448199710.jpg?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/hfe/h17/64208876634142.jpg?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/h35/h8f/84378448232478.jpg?format=gallery-large'
+    ],
+    rating:5,
+    url:"https://t.me/lab4webdev"
+    },
+    {
+      name:'Xiaomi Redmi Note 10 Pro 8 ГБ/256 ГБ серый',
+      description:'технология NFC Откройте для себя больше возможностей',
+      link:"https://kaspi.kz/shop/p/xiaomi-redmi-note-10-pro-8-gb-256-gb-seryi-107221005/?c=750000000",
+      imgURL:'https://resources.cdn-kaspi.kz/img/m/p/h94/h74/64487156645918.jpg?format=gallery-large',
+      gallery: [
+        'https://resources.cdn-kaspi.kz/img/m/p/h94/h74/64487156645918.jpg?format=gallery-large',
+        'https://resources.cdn-kaspi.kz/img/m/p/h0a/h82/64487159398430.jpg?format=gallery-large',
+        'https://resources.cdn-kaspi.kz/img/m/p/h2b/h95/64487160807454.jpg?format=gallery-large'
+      ],
+      rating:5,
+      url:"https://t.me/lab4webdev"
+    }
+    ,
+    {
+      name:'Apple iPhone 14 128Gb голубой',
+      description:'Стильный смартфон с мощным процессором, инновационной камерой, высокой производительностью и улучшенной автономностью для комфортного повседневного использования.',
+      link:"https://kaspi.kz/shop/p/apple-iphone-14-128gb-goluboi-106363150/?c=750000000",
+      imgURL:'https://resources.cdn-kaspi.kz/img/m/p/h6a/h15/86042945683486.png?format=gallery-large',
+      gallery: [
+        'https://resources.cdn-kaspi.kz/img/m/p/h6a/h15/86042945683486.png?format=gallery-large',
+        'https://resources.cdn-kaspi.kz/img/m/p/h5c/hb7/86042945716254.png?format=gallery-large',
+        'https://resources.cdn-kaspi.kz/img/m/p/h35/h8c/86042945781790.png?format=gallery-large'
+      ],
+      rating:5,
+      url:"https://t.me/lab4webdev"
+    },
+    {
+      name:'Dyson Airwrap HS05 Long с косметичкой и расческами',
+      description:'Многофункциональный стайлер для укладки волос с минимальным нагревом, стильной косметичкой и аксессуарами для создания профессионального образа.',
+      link:"https://kaspi.kz/shop/p/dyson-airwrap-hs05-long-s-kosmetichkoi-i-rascheskami-stailer-1300-vt-114187319/?c=750000000&sr=4&qid=105b2c8b9979f7a11c054f80281e257d&ref=shared_link",
+      imgURL:'https://resources.cdn-kaspi.kz/img/m/p/h31/hf9/84395425529886.jpg?format=gallery-large',
+      gallery: [
+        'https://resources.cdn-kaspi.kz/img/m/p/h31/hf9/84395425529886.jpg?format=gallery-large',
+        'https://resources.cdn-kaspi.kz/img/m/p/h0f/hb7/84395425726494.jpg?format=gallery-large',
+        'https://resources.cdn-kaspi.kz/img/m/p/hc9/h19/84395425595422.jpg?format=gallery-large'
+      ],
+      rating:5,
+      url:"https://t.me/lab4webdev"
+    },
+    {
+      name:'Игровая приставка Sony PlayStation 5 белый',
+      description:'Мощная консоль нового поколения с потрясающей графикой, ультрабыстрой загрузкой, поддержкой 4K и захватывающими играми для незабываемого гейминга.',
+      link:"https://kaspi.kz/shop/p/sony-playstation-5-belyi-100746577/?c=750000000&sr=8&qid=9438c745c78ad8adaf927ae6fbd1172d&ref=shared_link",
+      imgURL:'https://resources.cdn-kaspi.kz/img/m/p/h02/h59/63948652249118.jpg?format=gallery-large',
+      gallery: [
+        'https://resources.cdn-kaspi.kz/img/m/p/h02/h59/63948652249118.jpg?format=gallery-large',
+        'https://resources.cdn-kaspi.kz/img/m/p/h2b/h90/63948654936094.jpg?format=gallery-large',
+        'https://resources.cdn-kaspi.kz/img/m/p/ha3/h00/63948657491998.jpg?format=gallery-largehttps://resources.cdn-kaspi.kz/img/m/p/ha3/h00/63948657491998.jpg?format=gallery-large'
+      ],
+      rating:5,
+      url:"https://t.me/lab4webdev"
+    },
+    {
+      name:'Экшн-камера GoPro HERO13 Black + Accessories Bundle',
+      description:'Экшн-камера GoPro HERO13 Black с аксессуарами: невероятное качество съемки, 5K-видео, стабилизация HyperSmooth, водонепроницаемость, удобный комплект для активного отдыха и профессиональных приключений.',
+      link:"https://kaspi.kz/shop/p/ekshn-kamera-gopro-hero13-black-accessories-bundle-128879262/?c=750000000&sr=3&qid=36984823b7a19a2623f59b769fe37319&ref=shared_link",
+      imgURL:'https://resources.cdn-kaspi.kz/img/m/p/p91/p09/4501643.png?format=gallery-large',
+      gallery: [
+        'https://resources.cdn-kaspi.kz/img/m/p/p91/p09/4501643.png?format=gallery-large',
+        'https://resources.cdn-kaspi.kz/img/m/p/pca/p09/4501645.png?format=gallery-large'
+      ],
+      rating:5,
+      url:"https://t.me/lab4webdev"
+    },
+    {
+      name:'Ноутбук Apple MacBook Pro 16 2023 16.2" / 18 Гб / SSD 512 Гб / macOS / MRW13',
+      description:'Высокопроизводительный ноутбук с мощным процессором, великолепным дисплеем Retina, 18 ГБ оперативной памяти и SSD на 512 ГБ для продуктивной работы.',
+      link:"https://kaspi.kz/shop/p/apple-macbook-pro-16-2023-16-2-18-gb-ssd-512-gb-macos-mrw13-114863282/?c=750000000&m=6836017&sr=1&qid=9cc303b464ed2bdf6935412496846a06&isPromoted=true&ref=shared_link",
+      imgURL:'https://resources.cdn-kaspi.kz/img/m/p/he6/h06/84577687306270.png?format=gallery-large',
+      gallery: [
+        'https://resources.cdn-kaspi.kz/img/m/p/he6/h06/84577687306270.png?format=gallery-large',
+        'https://resources.cdn-kaspi.kz/img/m/p/h1c/h28/84577687240734.png?format=gallery-large',
+        'https://resources.cdn-kaspi.kz/img/m/p/hd0/h8a/84577687437342.png?format=gallery-large'
+      ],
+      rating:5,
+      url:"https://t.me/lab4webdev"
+    },
+    {
+      name:'Кофемашина Delonghi DeLonghi ECAM 290.51 B черный',
+      description:' Элегантная кофемашина для приготовления ароматного эспрессо и капучино, с настройками персонализации, автоматической очисткой и стильным дизайном.',
+      link:"https://kaspi.kz/shop/p/kofemashina-delonghi-delonghi-ecam-290-51-b-chernyi-132137874/?c=750000000",
+      imgURL:'https://resources.cdn-kaspi.kz/img/m/p/pda/p35/15662173.png?format=gallery-large',
+      gallery: [
+        'https://resources.cdn-kaspi.kz/img/m/p/pda/p35/15662173.png?format=gallery-large',
+        'https://resources.cdn-kaspi.kz/img/m/p/pa2/p35/15662175.jpg?format=gallery-large',
+        'https://resources.cdn-kaspi.kz/img/m/p/p85/p35/15662176.jpg?format=gallery-large'
+      ],
+      rating:5,
+      url:"https://t.me/lab4webdev"
+    },
+    {
+      name:'Ноутбук Apple MacBook Pro 16 2023 16.2" / 18 Гб / SSD 512 Гб / macOS / MRW13',
+      description:'Высокопроизводительный ноутбук с мощным процессором, великолепным дисплеем Retina, 18 ГБ оперативной памяти и SSD на 512 ГБ для продуктивной работы.',
+      link:"https://kaspi.kz/shop/p/apple-macbook-pro-16-2023-16-2-18-gb-ssd-512-gb-macos-mrw13-114863282/?c=750000000&m=6836017&sr=1&qid=9cc303b464ed2bdf6935412496846a06&isPromoted=true&ref=shared_link",
+      imgURL:'https://resources.cdn-kaspi.kz/img/m/p/he6/h06/84577687306270.png?format=gallery-large',
+      gallery: [
+        'https://resources.cdn-kaspi.kz/img/m/p/he6/h06/84577687306270.png?format=gallery-large',
+        'https://resources.cdn-kaspi.kz/img/m/p/h1c/h28/84577687240734.png?format=gallery-large',
+        'https://resources.cdn-kaspi.kz/img/m/p/hd0/h8a/84577687437342.png?format=gallery-large'
+      ],
+      rating:5,
+      url:"https://t.me/lab4webdev"
+    },
+    {
+      name:'Кофемашина Delonghi DeLonghi ECAM 290.51 B черный',
+      description:' Элегантная кофемашина для приготовления ароматного эспрессо и капучино, с настройками персонализации, автоматической очисткой и стильным дизайном.',
+      link:"https://kaspi.kz/shop/p/kofemashina-delonghi-delonghi-ecam-290-51-b-chernyi-132137874/?c=750000000",
+      imgURL:'https://resources.cdn-kaspi.kz/img/m/p/pda/p35/15662173.png?format=gallery-large',
+      gallery: [
+        'https://resources.cdn-kaspi.kz/img/m/p/pda/p35/15662173.png?format=gallery-large',
+        'https://resources.cdn-kaspi.kz/img/m/p/pa2/p35/15662175.jpg?format=gallery-large',
+        'https://resources.cdn-kaspi.kz/img/m/p/p85/p35/15662176.jpg?format=gallery-large'
+      ],
+      rating:5,
+      url:"https://t.me/lab4webdev"
+    }
+
+]
