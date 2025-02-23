6d351f542966b80935960da5b22a1523d7455b44
Marina <123238442+Marinqaa@users.noreply.github.com>
2025-02-23 18:48:04 +0500

    Add files via upload

---
+++ lab5/src/app/categories.ts
@@ -0,0 +1,23 @@
+export interface Category {
+    id: number;
+    name: string;
+}
+
+export const category = [
+    {
+        id: 1,
+        name: 'Electronics'
+    },
+    {
+        id: 2,
+        name: 'Home Appliances'
+    },
+    {
+        id: 3,
+        name: 'Books'
+    },
+    {
+        id: 4,
+        name: 'Fashion'
+    }
+]
